20600a4c3524af5bb5d2f9e3b73496a1b4b558eb
Liza-des <lizades@bk.ru>
2024-08-11 17:28:58 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+# Вариант 2
+first = int(input('1: '))
+second = int(input('2: '))
+third = int(input('3: '))
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
